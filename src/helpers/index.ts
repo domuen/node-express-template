db13e15657c259d7f5f1ce256cb68014ae39e7f2
Domenic Muentes <domenicmuentes0@gmail.com>
2022-12-13 11:02:49 -0500

    create index.ts for models and helpers

---
+++ src/helpers/index.ts
@@ -0,0 +1,5 @@
+export { default as LogRequest } from "./log-request";
+export { default as logger } from "./logger";
+export { default as ServerResponse } from "./server-response";
+export { default as VerifyApiKey } from "./verify-api-key";
+export { default as VerifyToken } from "./verify-token";
---
+++ src/models/index.ts
@@ -0,0 +1,1 @@
+export { default as M } from "./m";
