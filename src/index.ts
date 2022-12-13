90f51994519ec511e0c19b6ad41593d6418b8194
Domenic Muentes <domenicmuentes0@gmail.com>
2022-12-13 11:14:50 -0500

    lower body limit

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import logger from "./helpers/logger";
 const app = Express();
 const SERVICE_PORT = process.env.SERVICE_PORT ?? 0;
 
-app.use(Express.json({ limit: "20mb" }));
+app.use(Express.json({ limit: "8mb" }));
 router(app);
 
 app.listen(SERVICE_PORT, () => logger.verbose("Listening on port", SERVICE_PORT));
