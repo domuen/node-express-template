6ba2948c44e31e83bcb8d1a2c4e156a0d90d8f27
Domenic Muentes <domenicmuentes0@gmail.com>
2022-12-13 11:11:05 -0500

    remove schema import

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import "dotenv/config";
 import Express from "express";
 import router from "./router";
-import "./schemas/user-schema";
 import logger from "./helpers/logger";
 
 const app = Express();
-const { SERVICE_PORT } = process.env;
+const SERVICE_PORT = process.env.SERVICE_PORT ?? 0;
 
 app.use(Express.json({ limit: "20mb" }));
 router(app);
 
-app.listen(SERVICE_PORT ?? 0, () => logger.verbose("Listening on port", SERVICE_PORT));
+app.listen(SERVICE_PORT, () => logger.verbose("Listening on port", SERVICE_PORT));
