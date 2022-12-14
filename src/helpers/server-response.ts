00877be4a8689c02c2cd1204531a3f2f60563c3b
Domenic Muentes <domenicmuentes0@gmail.com>
2022-12-13 19:44:08 -0500

    remove unused helper

--- src/helpers/server-response.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from "express";
-
-const ServerResponse = (
-  res: Response,
-  status: number,
-  body?: any
-) => res.status(status).json(body).end();
-
-export default ServerResponse;
