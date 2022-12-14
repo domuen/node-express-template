9a1e98057cc09cc19039e8597f62dd95a36661af
Domenic Muentes <domenicmuentes0@gmail.com>
2022-12-13 19:33:15 -0500

    convert to class

--- src/helpers/controller.ts
+++ src/helpers/controller.ts
@@ -5,14 +5,16 @@ interface ControllerResponse {
   data?: any;
 }
 
-export default
+export default class Controller {
   // controller files will call this top function,
   // passing a callback which contains the controller code
 
   // the controller passed will return a 
   // status code and some data if applicable
-  (controller: (req: Request, res: Response, next: NextFunction) => Promise<ControllerResponse>) =>
+  constructor(controller: (req: Request, res: Response, next: NextFunction) => Promise<ControllerResponse>) {
     // This curried function is what app.<method> will receive
-    (req: Request, res: Response, next: NextFunction) =>
+    return (req: Request, res: Response, next: NextFunction) =>
       controller(req, res, next).then(response =>
         res.status(response.status).json(response.data).end());
+  }
+}
